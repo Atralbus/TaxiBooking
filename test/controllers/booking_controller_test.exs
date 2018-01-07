33b9db4f14ee2b8e31c7457ebb770cc3f4173055
ismgul <ismgul@ttu.ee>
2018-01-07 18:39:34 +0200

    tdd added for booking fields validation

--- test/controllers/booking_controller_test.exs
+++ test/controllers/booking_controller_test.exs
@@ -1,7 +1,22 @@
 defmodule Exam1.BookingControllerTest do
   use Exam1.ConnCase
 
-  alias Exam1.{Repo,Taxi}
+  alias Exam1.{Repo,Taxi,Booking}
+
+  test "booking requires a 'pickup address'" do
+    changeset = Booking.changeset(%Booking{}, %{pickup_address: nil, dropoff_address: "Liivi 2"})
+    assert Keyword.has_key? changeset.errors, :pickup_address
+  end
+
+  test "booking requires a 'dropoff address'" do
+    changeset = Booking.changeset(%Booking{}, %{pickup_address: "Lõunakeskus", dropoff_address: nil})
+    assert Keyword.has_key? changeset.errors, :dropoff_address
+  end
+
+  test "booking 'pickup address' and 'dropoff address' cannot be same" do
+    changeset = Booking.changeset(%Booking{}, %{pickup_address: "Lõunakeskus", dropoff_address: "Lõunakeskus"})
+    assert Keyword.has_key? changeset.errors, :difference
+  end
 
   test "Booking rejection", %{conn: conn} do
     Repo.insert!(%Taxi{status: "busy"})
--- web/models/booking.ex
+++ web/models/booking.ex
@@ -13,5 +13,20 @@ defmodule Exam1.Booking do
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, [:pickup_address, :dropoff_address])
+    |> validate_difference
+    |> validate_required([:pickup_address, :dropoff_address])
   end
+
+  def validate_difference(changeset) do
+    pickup = get_field(changeset, :pickup_address)
+    dropoff = get_field(changeset, :dropoff_address)
+
+    validate_difference(changeset, pickup, dropoff)
+  end
+
+  def validate_difference(changeset, pickup, dropoff) when pickup == dropoff do
+    add_error(changeset, :difference, "pickup and dropoff cannot be same")
+  end
+
+  def validate_difference(changeset, _, _), do: changeset
 end
