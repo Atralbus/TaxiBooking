4d6ef11f5a7c3cf719224bdcb99f4adee7c3cd65
ismgul <ismgul@ttu.ee>
2018-01-07 15:54:42 +0200

    hound added to white_bread context

--- features/contexts/white_bread_context.exs
+++ features/contexts/white_bread_context.exs
@@ -1,6 +1,19 @@
 defmodule WhiteBreadContext do
   use WhiteBread.Context
+  use Hound.Helpers
 
+  feature_starting_state fn  ->
+    Application.ensure_all_started(:hound)
+    %{}
+  end
+  scenario_starting_state fn _state ->
+    Hound.start_session
+    %{}
+  end
+  scenario_finalize fn _status, _state -> 
+    Hound.end_session
+  end
+  
   given_ ~r/^the following taxis are on duty$/, fn state ->
     {:ok, state}
   end
